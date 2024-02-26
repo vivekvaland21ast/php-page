a156083404700fdd705e9349a4815586e2127c9c
Vivek-Valand <131301501+Vivek-Valand@users.noreply.github.com>
2024-02-26 09:49:14 +0530

    Database

---
+++ Database/demo.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 26, 2024 at 05:17 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `demo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notes`
+--
+-- Error reading structure for table demo.notes: #1932 - Table &#039;demo.notes&#039; doesn&#039;t exist in engine
+-- Error reading data for table demo.notes: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `demo`.`notes`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notes2`
+--
+-- Error reading structure for table demo.notes2: #1932 - Table &#039;demo.notes2&#039; doesn&#039;t exist in engine
+-- Error reading data for table demo.notes2: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `demo`.`notes2`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+-- Error reading structure for table demo.post: #1932 - Table &#039;demo.post&#039; doesn&#039;t exist in engine
+-- Error reading data for table demo.post: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `demo`.`post`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table demo.users: #1932 - Table &#039;demo.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table demo.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `demo`.`users`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users2`
+--
+-- Error reading structure for table demo.users2: #1932 - Table &#039;demo.users2&#039; doesn&#039;t exist in engine
+-- Error reading data for table demo.users2: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `demo`.`users2`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
